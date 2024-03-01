781b17abc7ae78740a7d614d1115108c487f9f4a
theogab <43400888+theogab@users.noreply.github.com>
2024-03-01 14:17:56 +0100

    cleaning repo

--- Scripts/install.R
+++
@@ -1,3 +0,0 @@
-library(devtools)
-withr::with_libpaths(new = "/scratch/axiom/FAC/FBM/DBC/nsalamin/default/tgaboria/R", install_github('theogab/jive-package', force = T))
-library(jive)
--- Scripts/install.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -l
-
-module add Bioinformatics/Software/vital-it 
-module add R/latest
-Rscript /scratch/axiom/FAC/FBM/DBC/nsalamin/default/tgaboria/ABM/Scripts/install.R
-module rm R/latest
