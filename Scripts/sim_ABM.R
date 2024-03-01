2638a9ca5979ee9b748f4570198741ca67cf2812
theogab <43400888+theogab@users.noreply.github.com>
2024-03-01 09:51:14 +0100

    new simulations

--- Scripts/sim_ABM.R
+++ Scripts/sim_ABM.R
@@ -1,22 +1,27 @@
 ## Simulate data
 sim_ABM <- function(tree, pars_logX = c(.1, 2, -Inf, Inf), pars_m = c(.1, 10, -Inf, Inf), nu = 0.5, omega = 0.5, nsim = 1){
   
-  b.len <- tree$edge.length
-  e1 <- tree$edge[,1]
-  e2 <- tree$edge[,2]
-  n <- length(tree$tip.label)
-  
   # simulate data
   dat <- lapply(1:nsim, function(i){
+    
+    if(class(tree) == "multiPhylo"){
+      if(length(tree)!=nsim) stop("tree must be of the same length as nsim")
+      phy <- tree[[i]]
+    } else phy <- tree
+    
+    b.len <- phy$edge.length
+    e1 <- phy$edge[,1]
+    e2 <- phy$edge[,2]
+    n <- length(phy$tip.label)
     
     # standard BM variation
     var_logX <- rnorm(length(b.len), 0, sqrt(pars_logX[1]*b.len))
     var_m <- rnorm(length(b.len), 0, sqrt(pars_m[1]*b.len))
     
     # inheritance assymetry
-    Sm <- rep(0, n+tree$Nnode)
-    Sx <- rep(0, n+tree$Nnode)
-    for(j in 1:(n+tree$Nnode)){
+    Sm <- rep(0, n+phy$Nnode)
+    Sx <- rep(0, n+phy$Nnode)
+    for(j in 1:(n+phy$Nnode)){
       if(j %in% e2 & Sm[j] == 0 | Sx[j] == 0){
         anc <- e1[e2 == j]
         sis <- e2[e1 == anc]
@@ -37,9 +42,9 @@ sim_ABM <- function(tree, pars_logX = c(.1, 2, -Inf, Inf), pars_m = c(.1, 10, -I
     }
     
     # go from roots to tips
-    m <- c(rep(0, n), pars_m[2], rep(0, tree$Nnode-1))
-    logX <- c(rep(0, n), pars_logX[2], rep(0, tree$Nnode-1))
-    order <- c(n + (2:tree$Nnode), 1:n)
+    m <- c(rep(0, n), pars_m[2], rep(0, phy$Nnode-1))
+    logX <- c(rep(0, n), pars_logX[2], rep(0, phy$Nnode-1))
+    order <- c(n + (2:phy$Nnode), 1:n)
     for(desc in order){
       anc <- e1[e2 == desc]
       bn <- which(e2 == desc)
